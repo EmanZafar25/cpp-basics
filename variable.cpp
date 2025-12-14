fd0f69bee0bfc9f9b28234cfb0726c2b9675d931
Eman Zafar <emanzafar482@gmail.com>
2025-12-14 21:18:08 +0500

    Add basic user input for age and height and gives output

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    float height;
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Enter height: ";
+    cin >> height;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    return 0;
+}
